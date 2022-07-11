5b8aed11a21d25ca2a0319ba41d2b8429459be12
Yeray Darias <ydarias@gmail.com>
2022-07-11 17:11:57 +0200

    Added AWS Lambda scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const message = 'Hello world';
-console.log(message);
+import {APIGatewayEvent, APIGatewayProxyCallback, Context} from 'aws-lambda';
+
+export const lambdaHandler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback): void => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'hello world',
+    }),
+  });
+};
