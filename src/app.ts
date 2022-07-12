b78a51681bc2780c150cddd6f709e14a549074db
Yeray Darias <ydarias@gmail.com>
2022-07-12 17:09:10 +0100

    Added puppeteer code again

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,52 @@
 import {APIGatewayEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
+import {Browser} from 'puppeteer-core';
+import chromium from 'chrome-aws-lambda';
 
 exports.handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: 'This is another output',
-  };
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+  let browser: Browser | undefined;
+
+  try {
+    console.log('creating browser ...');
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: false,
+      ignoreHTTPSErrors: true,
+    });
+
+    console.log('creating a new page ...');
+    const page = await browser.newPage();
+
+    console.log('navigating to Google ...');
+    await page.goto('https://google.com');
+
+    const result = await page.title();
+
+    console.log(`Title: ${result}`);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: result,
+      }),
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'error',
+      }),
+    };
+  } finally {
+    if (browser) {
+      console.log('Closing browser instance ...');
+      await browser.close();
+    }
+  }
 };
