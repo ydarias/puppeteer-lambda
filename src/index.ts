59430734812e167c5310178233f317f7fe9b328b
Yeray Darias <ydarias@gmail.com>
2022-07-12 15:13:38 +0100

    Testing simple lambda

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,13 @@
-import {APIGatewayEvent, APIGatewayProxyCallback, Context} from 'aws-lambda';
-import chromium from 'chrome-aws-lambda';
-import {Browser} from 'puppeteer-core';
+import {APIGatewayEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
 
-export const lambdaHandler = async (
-  event: APIGatewayEvent,
-  context: Context,
-  callback: APIGatewayProxyCallback,
-): Promise<void> => {
+export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
-  return callback(null, {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'success',
     }),
-  });
-
-  // let browser: Browser | undefined;
-  //
-  // try {
-  //   browser = await chromium.puppeteer.launch({
-  //     args: chromium.args,
-  //     defaultViewport: chromium.defaultViewport,
-  //     executablePath: await chromium.executablePath,
-  //     headless: chromium.headless,
-  //     ignoreHTTPSErrors: true,
-  //   });
-  //
-  //   const page = await browser.newPage();
-  //
-  //   await page.goto('https://google.com');
-  //
-  //   const result = await page.title();
-  //
-  //   console.log(`Title: ${result}`);
-  //
-  //   return callback(null, {
-  //     statusCode: 200,
-  //     body: JSON.stringify({
-  //       message: result,
-  //     }),
-  //   });
-  // } catch (e) {
-  //   return callback(e as Error);
-  // } finally {
-  //   if (browser) {
-  //     await browser.close();
-  //   }
-  // }
+  };
 };
