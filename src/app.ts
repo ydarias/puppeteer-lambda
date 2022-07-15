d121ca5db9e001658678ebab62c1f9dba051fd49
Yeray Darias <ydarias@gmail.com>
2022-07-15 11:35:51 +0100

    Environment variables log print

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,6 @@ exports.handler = async (event: APIGatewayEvent, context: Context): Promise<APIG
 
   try {
     console.log('configuring the proxy URL ...');
-    console.log(`Process is ${JSON.stringify(process, null, 4)}`);
     console.log(`ENV is ${JSON.stringify(process.env, null, 4)}`);
     const proxyURL = process.env.BRIGHT_DATA_PROXY || '';
     console.log(`Using proxy ${proxyURL}`);
